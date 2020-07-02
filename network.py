1093cc5acea9cc3516262d657ae6828547ca89df
Toby Boyne <tobyboyne@gmail.com>
2020-07-02 18:38:59 +0100

    Wrote training loop

--- network.py
+++ network.py
@@ -4,36 +4,44 @@ import torch.nn.functional as F
 
 
 class Net(nn.Module):
-
     def __init__(self):
         super(Net, self).__init__()
-        # 1 input image channel, 6 output channels, 3x3 square convolution
-        # kernel
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        # an affine operation: y = Wx + b
-        self.fc1 = nn.Linear(16 * 6 * 6, 120)  # 6*6 from image dimension
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        # Max pooling over a (2, 2) window
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        # If the size is a square you can only specify a single number
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+    def get_target(self, labels):
+        target = torch.zeros([len(labels), 10])
+        for i, n in enumerate(labels):
+            target[i, n] = 1.0
+        return target
+
+    # def train_loop(self, inputs, labels):
+    #     self.optim.zero_grad()  # zero the gradient buffers
+    #     output = net(x_in)
+    #     target = self.get_target(target_n)
+    #     loss = loss_fn(output, target)
+    #     loss.backward()
+    #     self.optim.step()
 
 
-net = Net()
-print(net)
+
+if __name__ == "__main__":
+    net = Net()
+    print(net)
+
+    x_in = torch.randn(1, 1, 28, 28)
+    net.train_loop(x_in, 1)
+    print(net.conv2.bias.grad)
--- training.py
+++ training.py
@@ -2,36 +2,65 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-
 import matplotlib.pyplot as plt
 import numpy as np
 
+from network import Net
+
 def imshow(img):
-	# img = img / 2 + 0.5     # unnormalize
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
 	plt.show()
 
 transform = transforms.Compose(
-	[transforms.ToTensor()])
-	#  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+	[transforms.ToTensor()]
+)
 
 trainset = torchvision.datasets.MNIST("./training-data", train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
 										  shuffle=True, num_workers=2)
+
 testset = torchvision.datasets.MNIST("./training-data", train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 										 shuffle=False, num_workers=2)
 
 
 
-# print images
 if __name__ == "__main__":
-	print(type(testloader))
-	data_iter = iter(testloader)
-	# data_iter.next() takes next batch (size=4)
-	images, labels = data_iter.next()
-	imshow(torchvision.utils.make_grid(images))
-	# for i, element in enumerate(data_iter):
-	# 	if i == 4: break
-	# 	imshow(element)
+	# data_iter = iter(testloader)
+	# # data_iter.next() takes next batch (size=4)
+	# images, labels = data_iter.next()
+	# print(labels)
+	# imshow(torchvision.utils.make_grid(images, padding=0))
+
+	# create new network, optimizer, and loss criterion
+	net = Net()
+	optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
+	loss_fn = torch.nn.MSELoss()
+
+	# training loop
+	for epoch in range(2):
+
+		running_loss = 0.0
+		for i, data in enumerate(trainloader):
+			# get the inputs; data is a list of [inputs, labels]
+			inputs, labels = data
+
+			# zero the parameter gradients
+			optimizer.zero_grad()
+
+			# forward + backward + optimize
+			outputs = net(inputs)
+			target = net.get_target(labels)
+			loss = loss_fn(outputs, target)
+			loss.backward()
+			optimizer.step()
+
+			# print statistics
+			running_loss += loss.item()
+			if i % 2000 == 1999:  # print every 2000 mini-batches
+				print('[%d, %5d] loss: %.3f' %
+					  (epoch + 1, i + 1, running_loss / 2000))
+				running_loss = 0.0
+
+	print('Finished Training')
