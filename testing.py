d6267eb1108899de4ee8ac28a39d994e09a2905a
Toby Boyne <tobyboyne@gmail.com>
2020-07-03 15:32:40 +0100

    Added testing of dataset

---
+++ testing.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision
+
+from tqdm import tqdm
+
+from network import Net
+from training import PATH, transform
+
+testset = torchvision.datasets.MNIST("./training-data", train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+										 shuffle=False, num_workers=2)
+
+if __name__ == "__main__":
+	net = Net()
+	net.load_state_dict(torch.load(PATH))
+
+	correct = 0
+	total = 0
+	with torch.no_grad():
+		with tqdm(testloader) as tqdm_iterator:
+			for data in tqdm_iterator:
+				images, labels = data
+				outputs = net(images)
+				_, predicted = torch.max(outputs.data, 1)
+				total += labels.size(0)
+				correct += (predicted == labels).sum().item()
+
+	print(f'Accuracy of the network on the {total} test images: {100 * correct / total}%')
--- training.py
+++ training.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 
 import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
 from network import Net
 
@@ -11,6 +12,8 @@ def imshow(img):
 	npimg = img.numpy()
 	plt.imshow(np.transpose(npimg, (1, 2, 0)))
 	plt.show()
+
+PATH = './models/mnist_net.pth'
 
 transform = transforms.Compose(
 	[transforms.ToTensor()]
@@ -20,9 +23,6 @@ trainset = torchvision.datasets.MNIST("./training-data", train=True, download=Tr
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
 										  shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.MNIST("./training-data", train=False, download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-										 shuffle=False, num_workers=2)
 
 
 
@@ -33,34 +33,37 @@ if __name__ == "__main__":
 	# print(labels)
 	# imshow(torchvision.utils.make_grid(images, padding=0))
 
+
 	# create new network, optimizer, and loss criterion
 	net = Net()
 	optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
 	loss_fn = torch.nn.MSELoss()
 
 	# training loop
-	for epoch in range(2):
+	for epoch in range(1):
 
 		running_loss = 0.0
-		for i, data in enumerate(trainloader):
-			# get the inputs; data is a list of [inputs, labels]
-			inputs, labels = data
+		i = 0
+		with tqdm(trainloader) as tqdm_iterator:
+			for data in tqdm_iterator:
+				# get the inputs; data is a list of [inputs, labels]
+				inputs, labels = data
 
-			# zero the parameter gradients
-			optimizer.zero_grad()
+				# zero the parameter gradients
+				optimizer.zero_grad()
 
-			# forward + backward + optimize
-			outputs = net(inputs)
-			target = net.get_target(labels)
-			loss = loss_fn(outputs, target)
-			loss.backward()
-			optimizer.step()
+				# forward + backward + optimize
+				outputs = net(inputs)
+				target = net.get_target(labels)
+				loss = loss_fn(outputs, target)
+				loss.backward()
+				optimizer.step()
 
-			# print statistics
-			running_loss += loss.item()
-			if i % 2000 == 1999:  # print every 2000 mini-batches
-				print('[%d, %5d] loss: %.3f' %
-					  (epoch + 1, i + 1, running_loss / 2000))
-				running_loss = 0.0
+				tqdm_iterator.set_description(f"{loss:5f}")
+				# i += 1
+				# if i > 1000:
+				# 	break
 
 	print('Finished Training')
+
+	torch.save(net.state_dict(), PATH)
