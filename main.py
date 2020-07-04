4ace5cdc9fc408e44a33c42a3f1d9082348da309
Toby Boyne <tobyboyne@gmail.com>
2020-07-04 16:39:19 +0100

    Improved UI (added clearing/better updating)

--- main.py
+++ main.py
@@ -6,21 +6,16 @@ from testing import load_model
 from user_input import DrawUI
 
 def solve_event_fn(draw_ui, network):
-	def f(x):
+	def f(event):
 		inputs = torch.from_numpy(draw_ui.drawing)
 		inputs = inputs.unsqueeze(0)
 		inputs = inputs.unsqueeze(0).float()
 		outputs = network.forward(inputs)
 		n = torch.argmax(outputs)
-		print(n)
+		print(n.item())
 	return f
 
 
 if __name__ == "__main__":
 	net = load_model()
-	draw = DrawUI()
-
-	f = solve_event_fn(draw, net)
-	draw.fig.canvas.mpl_connect('key_press_event', f)
-
-	plt.show()
+	draw = DrawUI(key_fn=solve_event_fn, network=net)
--- user_input.py
+++ user_input.py
@@ -2,6 +2,8 @@ import torch
 
 import matplotlib.pyplot as plt
 import numpy as np
+from time import perf_counter
+
 
 def get_brush_mask(x, y, r=5):
 	"""Get a circular brush as a numpy array
@@ -12,21 +14,42 @@ def get_brush_mask(x, y, r=5):
 	return brush
 
 class DrawUI:
-	def __init__(self, size=28):
+	def __init__(self, size=28, key_fn=None, network=None):
 		self.size = size
 		self.drawing = np.zeros((size, size))
 		self.fig, ax = plt.subplots()
 		self.fig.canvas.mpl_connect('motion_notify_event', self.draw)
+		self.fig.canvas.mpl_connect('button_press_event', self.clear)
+		if key_fn and network:
+			self.fig.canvas.mpl_connect('key_press_event', key_fn(self, network))
 
 		self.image = ax.imshow(self.drawing, cmap="gray", vmin=0, vmax=1)
 
+
+		timer = self.fig.canvas.new_timer(interval=50)
+		timer.add_callback(self.update, ax)
+		timer.start()
+		plt.show()
+
+	def add_space_fn(self, func):
+		self.fig.canvas.mpl_connect('key_press_event', func)
+
+
 	def draw(self, event):
 		if event.button == 1:  # left-click
+			t0 = perf_counter()
 			x, y = event.xdata, event.ydata
 			brush_mask = get_brush_mask(x, y)
 			self.drawing = np.maximum(brush_mask, self.drawing)
-			self.image.set_data(self.drawing)
-			self.fig.canvas.draw()
+			print(perf_counter() - t0)
+
+	def clear(self, event):
+		if event.dblclick:
+			self.drawing = np.zeros((self.size, self.size))
+
+	def update(self, ax):
+		self.image.set_data(self.drawing)
+		self.fig.canvas.draw()
 
 if __name__ == "__main__":
 	draw = DrawUI()
